4ed55b350fd2440dad71fb5fac5c21cf0a46c458
Tom Zellman <tzellman@github.org>
2010-11-30 11:34:59 +0000

    updated banner
    git-svn-id: http://svn.code.sf.net/p/coda-oss/code/trunk@215 1dbd42e5-0d20-4692-8756-183d5fc073b5

--- modules/c++/include/TestCase.h
+++ modules/c++/include/TestCase.h
@@ -1,10 +1,10 @@
 /* =========================================================================
- * This file is part of NITRO
+ * This file is part of CODA
  * =========================================================================
  *
  * (C) Copyright 2004 - 2010, General Dynamics - Advanced Information Systems
  *
- * NITRO is free software; you can redistribute it and/or modify
+ * CODA is free software; you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
  * the Free Software Foundation; either version 3 of the License, or
  * (at your option) any later version.
@@ -15,7 +15,7 @@
  * GNU Lesser General Public License for more details.
  *
  * You should have received a copy of the GNU Lesser General Public
- * License along with this program; if not, If not,
+ * License along with this program; If not,
  * see <http://www.gnu.org/licenses/>.
  *
  */
