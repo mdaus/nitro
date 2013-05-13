5274f29b2368644be8b662b0e8b581fbc320701c
Adam Sylvester <adam.sylvester@mdaus.com>
2013-05-13 12:17:11 +0000

    Fixed typo - env['install_bindir'] should get set from options.bindir, not options.libdir.  #238
    git-svn-id: http://svn.code.sf.net/p/coda-oss/code/trunk@673 1dbd42e5-0d20-4692-8756-183d5fc073b5

--- modules/build/build.py
+++ modules/build/build.py
@@ -1064,7 +1064,7 @@ def configure(self):
 
     env['install_includedir'] = Options.options.includedir if Options.options.includedir else join(Options.options.prefix, 'include')
     env['install_libdir'] = Options.options.libdir if Options.options.libdir else join(Options.options.prefix, 'lib')
-    env['install_bindir'] = Options.options.libdir if Options.options.bindir else join(Options.options.prefix, 'bin')
+    env['install_bindir'] = Options.options.bindir if Options.options.bindir else join(Options.options.prefix, 'bin')
     env['install_sharedir'] = Options.options.sharedir if Options.options.sharedir else join(Options.options.prefix, 'share')
 
     if Options.options.cxxflags:
