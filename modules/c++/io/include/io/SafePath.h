eea3a08c89ca0eec2dc9ad2318975f1e0d733c19
Hayden Spitzley <hspitzle@github.org>
2014-06-11 17:01:29 +0000

    Changed some comments. Ticket #386
    git-svn-id: http://svn.code.sf.net/p/coda-oss/code/trunk@810 1dbd42e5-0d20-4692-8756-183d5fc073b5

--- modules/c++/io/include/io/SafePath.h
+++ modules/c++/io/include/io/SafePath.h
@@ -29,7 +29,7 @@ namespace io
 {
 
 /*!
- * \brief TempFile class
+ * \brief SafePath class
  *
  * Class to protect against writing a broken file which
  * appears to be complete
@@ -39,12 +39,12 @@ class SafePath
 public:
 
     /*!
-     * Create a TempFile object and generate a unique, temporary name
+     * Create a SafePath object and generate a unique, temporary name
      */
     SafePath(const std::string& realPathname);
 
     /*!
-     * Destruct TempFile object
+     * Destruct SafePath object
      * This either changes the name of the file back to realName or deletes
      * the temp file depending on whether or not the moved flag has been set
      */
