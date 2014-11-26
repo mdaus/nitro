f86e98cce277f4ca3837e6b371e08191fa19f336
Adam Sylvester <adam.sylvester@mdaus.com>
2014-11-26 13:03:48 +0000

    Updating copyright banner to MDA
    git-svn-id: http://svn.code.sf.net/p/coda-oss/code/trunk@906 1dbd42e5-0d20-4692-8756-183d5fc073b5

--- modules/c++/math.poly/unittests/test_2d_poly.cpp
+++ modules/c++/math.poly/unittests/test_2d_poly.cpp
@@ -2,7 +2,7 @@
  * This file is part of math.poly-c++
  * =========================================================================
  *
- * (C) Copyright 2004 - 2009, General Dynamics - Advanced Information Systems
+ * (C) Copyright 2004 - 2014, MDA Information Systems LLC
  *
  * math.poly-c++ is free software; you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
