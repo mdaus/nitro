0eb8a663d5e7247ca582adee23c21fd5fa667288
Clyde Stanfield <clyde.stanfield@mdaus.com>
2014-12-10 17:56:25 +0000

    Updated dumplib to recurse properly and output libs in the correct order.
    git-svn-id: http://svn.code.sf.net/p/coda-oss/code/trunk@907 1dbd42e5-0d20-4692-8756-183d5fc073b5

--- modules/build/dumplib.py
+++ modules/build/dumplib.py
@@ -6,14 +6,17 @@ class dumplib(BuildContext):
     fun = 'build'
 
     def execute(self):
+
         # Recurse through all the wscripts to find all available tasks
         self.restore()
         if not self.all_envs:
             self.load_envs()
-        self.recurse([self.run_dir])
 
+        self.recurse([self.run_dir])
+        
         targets = self.targets.split(',')
         libs = []
+        
         for target in targets:
             # Find the target
             tsk = self.get_tgen_by_name(target)
@@ -21,25 +24,48 @@ class dumplib(BuildContext):
             # Actually create the task object
             tsk.post()
 
-            # Now we can grab his defines
+            # Now we can grab his libs
             libs += tsk.env.STLIB
-            
-            # Add use libs
-            for uselib in tsk.uselib:
-                if tsk.env['LIB_' + uselib]:
-                    libs += tsk.env['LIB_' + uselib]
+                
+        # Now run again but add all the targets we found
+        # This resolves running with multiple targets
+        moduleDeps = ''
+        for lib in libs:
+            moduleDeps += str(lib).split('-')[0] + ' '
 
-        # Sort and uniquify it, then print them all out
-        libs = sorted(list(set(libs)))
+        # Add in the original targets again
+        for target in targets:
+            moduleDeps += str(target).split('-')[0] + ' '
+
+        # Now run with the new module deps
+        modArgs = globals()
+        modArgs['NAME'] = 'dumplib'
+        modArgs['MODULE_DEPS'] = moduleDeps
+        
+        # We need a source file here so it doesn't think it is headers only
+        modArgs['SOURCE_DIR'] = 'build'
+        modArgs['SOURCE_EXT'] = 'pyc'
+        self.module(**modArgs)
+
+        self.recurse([self.run_dir])
+        
+        libs = []
+        tsk = self.get_tgen_by_name('dumplib-c++')
+        tsk.post()
+        libs = tsk.env.STLIB
+        
+        for uselib in tsk.uselib:
+            if tsk.env['LIB_' + uselib]:
+                libs += tsk.env['LIB_' + uselib]
         
         if len(libs) == 0:
             # If we found nothing print that we found nothing
             # otherwise it looks like the command failed.
             print 'No dependencies.'
         else:
-            str = ''
+            ret = ''
             for lib in libs:
-                if str:
-                    str += ' '
-                str += tsk.env.STLIB_ST % lib
-            print str
+                if ret:
+                    ret += ' '
+                ret += tsk.env.STLIB_ST % lib
+            print ret
