bfd681d0e82e95b6955f0367527667d94551d55f
anna-dodd <anna.dodd@mdaus.com>
2015-05-19 16:02:08 -0400

    Update jenkins.py

--- externals/coda-oss/build/scripts/jenkins.py
+++ externals/coda-oss/build/scripts/jenkins.py
@@ -66,7 +66,7 @@ elif '-vc10' in os.environ.get('JOB_NAME'):
     config_options += ["--msvc_version=msvc 10.0,msvc 10.0Exp"]
 
 print 'Job: %s' % os.environ.get('JOB_NAME', '')
-print "Revision: %s" % os.environ.get('SVN_REVISION', '')
+print "Revision: %s" % os.environ.get('GIT_COMMIT', '')
 print "LD_LIBRARY_PATH: %s" % os.environ.get('LD_LIBRARY_PATH','')
 
 install_path = installPath(package_name)
