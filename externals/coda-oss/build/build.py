0f408eb6eb89e741ffc084a5fd76d30deee6e2c9
anna-dodd <anna.dodd@mdaus.com>
2015-05-21 10:06:37 -0400

    Update build.py

--- externals/coda-oss/build/build.py
+++ externals/coda-oss/build/build.py
@@ -549,6 +549,7 @@ class CPPContext(Context.Context):
             if source:
                 print "Source path! : %s" % source
                 name = splitext(split(source)[1])[0]
+                print "Name! : %s" % name
 
             mex = bld(features='%s %sshlib'%(libExeType, libExeType), target=targetName or name,
                                    name=name, use=uselib_local,
