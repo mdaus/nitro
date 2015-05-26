91089bf22986a1a425beef97c9e0718952a30e44
Clyde Stanfield <clyde.stanfield@mdaus.com>
2015-05-26 09:28:00 -0400

    Win32 read now calculates bytesToRead with a size_t

--- modules/c++/sys/source/FileWin32.cpp
+++ modules/c++/sys/source/FileWin32.cpp
@@ -56,15 +56,14 @@ void sys::File::create(const std::string& str,
 
 void sys::File::readInto(char *buffer, Size_T size)
 {
-    static const DWORD MAX_READ_SIZE = std::numeric_limits<DWORD>::max();
+    static const size_t MAX_READ_SIZE = std::numeric_limits<DWORD>::max();
     size_t bytesRead = 0;
     size_t bytesRemaining = size;
 
     while (bytesRead < size)
     {
         // Determine how many bytes to read
-        const DWORD bytesToRead =
-            std::min<DWORD>(MAX_READ_SIZE, bytesRemaining);
+                std::min<size_t>(MAX_READ_SIZE, bytesRemaining));
 
         // Read from file
         DWORD bytesThisRead = 0;
