a67df1fd39570f7b09f307c36ed55a2fde313512
anna-dodd <anna.dodd@mdaus.com>
2015-06-22 10:42:04 -0400

    Update jenkins.py

--- build/scripts/jenkins.py
+++ build/scripts/jenkins.py
@@ -29,11 +29,13 @@ build_dir = options.build_dir
 if options.config_options is not None:
     config_options = options.config_options.split(',')
 else:
-    config_options = ["--enable-swig"]
+    config_options = []
 if options.build_options is not None:
     build_options = options.build_options.split(',')
 else:
     build_options = []
+    
+config_options.append("--enable-swig")
 
 print 'Package Name: %s' % package_name
 print 'Build Dir: %s' % build_dir
