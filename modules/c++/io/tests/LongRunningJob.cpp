16ce33d2e16afdb385c6bb038fc6519db6a77305
Micah.Bojrab <micah.bojrab@mdaus.com>
2015-09-10 13:37:23 -0400

    updating to work on Linux

--- modules/c++/io/tests/LongRunningJob.cpp
+++ modules/c++/io/tests/LongRunningJob.cpp
@@ -1,4 +1,8 @@
+#ifdef WIN32
 #include <windows.h>
+#else
+#include <unistd.h>
+#endif
 #include <iostream>
 #include <str/Convert.h>
 
@@ -7,7 +11,11 @@ int main(int argc, char **argv)
     for (size_t ii = 0; ii < 1000; ++ii)
     {
         std::cout << "Wait [" << str::toString(ii) << "] Finished!" << std::endl;
+#ifdef WIN32
         Sleep(1000);
+#else
+        sleep(1);
+#endif
     }
     return 0;
 }
