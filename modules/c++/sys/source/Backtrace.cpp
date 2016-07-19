d351c6a70582e9e8a83dbd1c58b96edecba0ed00
Charles Vink <charles.vink@mdaus.com>
2016-07-19 14:05:01 -0400

    Added utility function to show a backtrace on glibc systems

---
+++ modules/c++/sys/source/Backtrace.cpp
@@ -0,0 +1,37 @@
+#include <sys/Backtrace.h>
+
+#include <sys/sys_config.h>
+#include <sstream>
+
+const size_t MAX_STACK_ENTRIES = 62;
+
+#ifdef HAVE_EXECINFO_H
+
+#include <execinfo.h>
+#include <cstdlib>
+
+std::string sys::getBacktrace()
+{
+    void * stackBuffer[MAX_STACK_ENTRIES];
+    int currentStackSize = backtrace(stackBuffer, MAX_STACK_ENTRIES);
+    char ** stackSymbols = backtrace_symbols(stackBuffer, currentStackSize);
+
+    std::stringstream ss;
+    for (int ii = 0; ii < currentStackSize; ++ii)
+    {
+        ss << stackSymbols[ii] << std::endl;
+    }
+    std::free(stackSymbols);
+
+    return ss.str();
+}
+
+#else
+
+std::string sys::getBacktrace()
+{
+    return "sys::getBacktrace() is not supported " +
+        "on the current platform and/or libc";
+}
+
+#endif
