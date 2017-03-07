01df5466b43f71d412aa55061cce83796fe8db84
JonathanMeans <jonathan.means@mdaus.com>
2017-03-07 08:10:13 -0500

    Add copyright

--- modules/c++/sys/unittests/test_NaN_testing.cpp
+++ modules/c++/sys/unittests/test_NaN_testing.cpp
@@ -1,5 +1,27 @@
-#include "TestCase.h"
+/* =========================================================================
+ * This file is part of sys-c++
+ * =========================================================================
+ *
+ * (C) Copyright 2004 - 2017, MDA Information Systems LLC
+ *
+ * sys-c++ is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this program; If not,
+ * see <http://www.gnu.org/licenses/>.
+ *
+ */
+
 #include <limits>
+#include "TestCase.h"
 
 namespace
 {
@@ -15,12 +37,12 @@ TEST_CASE(testNaNsAreNotEqual)
     TEST_ASSERT_NOT_EQ(std::numeric_limits<float>::quiet_NaN(), 3.4);
 
 }
+
 TEST_CASE(testNaNIsNotAlmostEqualToNumber)
 {
     // Uncomment the test to see it work.
     // These test macros are supposed to fail here.
     // But I don't have a way to intercept the failure.
-    // the failure.
     //
     /*
     TEST_ASSERT_ALMOST_EQ(std::numeric_limits<float>::quiet_NaN(), 5);
