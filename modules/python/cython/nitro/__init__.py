5b44f096bc94b8bc99d3754d0130b2e3ed292a93
Trevor R.H. Clarke <tclarke@ball.com>
2018-02-14 10:14:12 -0500

    Automatically include nitro plugins.

--- modules/python/cython/nitro/__init__.py
+++ modules/python/cython/nitro/__init__.py
@@ -1,3 +1,7 @@
+import os
+if 'NITF_PLUGIN_PATH' not in os.environ:
+    from pkg_resources import resource_filename
+    os.environ['NITF_PLUGIN_PATH'] = resource_filename(__name__, 'plugins')
 from .dataextension_segment import *
 from .field import *
 from .error import *
--- modules/python/cython/setup.py
+++ modules/python/cython/setup.py
@@ -1,7 +1,9 @@
-from setuptools import setup
+from glob import glob
+from setuptools import setup, find_packages
 from setuptools.extension import Extension
 from Cython.Build import cythonize
 import numpy
+import os
 
 opts = {'extra_compile_args': ["-g"],
         'extra_link_args': ["-g"],
@@ -21,11 +23,17 @@ ext_modules = [
     Extension("nitro.types", sources=["types.pyx"], **opts),
 ]
 
+nitro_root = os.environ.get('NITRO_ROOT', os.path.abspath('../../../install'))
+if not os.path.exists("nitro/plugins"):
+    print("Linking plugins into package nitro")
+    os.symlink(os.path.join(nitro_root, "share/nitf/plugins"), "nitro/plugins")
+
 setup(
     name="nitro",
     version="0.2dev2",
-    packages=["nitro"],
-    install_requires=["numpy>=1.14.0", "Deprecated>=1.1.0"],
+    packages=find_packages(),
+    install_requires=["numpy>=1.13.0", "Deprecated>=1.1.0"],
+    include_package_data=True,
     ext_modules=cythonize(ext_modules, gdb_debug=True)
 )
 
