22f5d8d1c1306a37fb0edf9fba76c33e44db48f4
Trevor R.H. Clarke <tclarke@ball.com>
2018-03-30 15:39:17 -0400

    Tag a new dev release

--- modules/python/cython/setup.py
+++ modules/python/cython/setup.py
@@ -30,7 +30,7 @@ if not os.path.exists("nitro/plugins"):
 
 setup(
     name="nitro",
-    version="0.2.dev4",
+    version="0.3.dev1",
     packages=find_packages(),
     install_requires=["numpy>=1.13.0", "Deprecated>=1.1.0"],
     include_package_data=True,
