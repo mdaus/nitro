6d32f31dfbe42a756061454c39ddc363b06ce7e3
Scott Colcord <39102579+scolcord@users.noreply.github.com>
2018-08-31 15:46:47 -0400

    Add support for building most drivers on Linux.
    Implement CMake build support for most of the external drivers.
    
    Note that unlike the waf-based builds, we call out to those
    drivers' own build systems, instead of re-implementing them.
    
    These changes have probably broken the Windows builds, and should
    be tested there before being merged to the mainline.
    
    See CMakeREADME.txt for details.

--- build/config_tests.cmake
+++ build/config_tests.cmake
@@ -2,8 +2,26 @@ include(CheckIncludeFiles)
 include(CheckTypeSize)
 include(CheckSymbolExists)
 include(CheckLibraryExists)
-include(TestBigEndian)
 include(CheckCXXSourceCompiles)
+
+include(TestBigEndian)
+include(FindBoost)
+include(FindThreads)
+
+include(FindCURL)
+if (${CMAKE_VERSION} VERSION_LESS "3.12.0")
+#xxx FindCurl didn't create a target until CMake 3.12
+    if(CURL_FOUND)
+        if(NOT TARGET CURL::libcurl)
+            add_library(CURL::libcurl UNKNOWN IMPORTED)
+            set_target_properties(CURL::libcurl PROPERTIES INTERFACE_INCLUDE_DIRECTORIES "${CURL_INCLUDE_DIRS}")
+            set_property(TARGET CURL::libcurl APPEND PROPERTY IMPORTED_LOCATION "${CURL_LIBRARY}")
+        endif()
+    endif()
+endif()
+
+
+#include(FindZLIB)
 
 # Configure compiler checks.
 # These work in conjunction with the *_config.h.cmake.in files in the
@@ -11,6 +29,13 @@ include(CheckCXXSourceCompiles)
 check_include_files("pthread.h"						HAVE_PTHREAD_H)
 check_include_files("execinfo.h"					HAVE_EXECINFO_H)
 check_symbol_exists("clock_gettime"		"time.h"	HAVE_CLOCK_GETTIME)
+if (NOT HAVE_CLOCK_GETTIME) # On old systems this was in librt, not libc
+	unset("HAVE_CLOCK_GETTIME" CACHE) # check_xxx_exists set CACHE variables, which cannot be re-used without being unset.
+	check_library_exists(rt clock_gettime "time.h" 	HAVE_CLOCK_GETTIME)
+	if (HAVE_CLOCK_GETTIME) # Record the necessary extra link library
+		set(CLOCK_GETTIME_EXTRALIBS "rt" CACHE INTERNAL "")
+	endif()
+endif()
 check_include_files("atomic.h"						HAVE_ATOMIC_H)
 check_include_files("sys/time.h"					HAVE_SYS_TIME_H)
 check_symbol_exists("localtime_r"		"time.h"	HAVE_LOCALTIME_R)
@@ -22,10 +47,16 @@ test_big_endian(									BIGENDIAN)
 check_type_size(	"size_t"						SIZEOF_SIZE_T)
 
 #xxxTODO: Test this
-check_library_exists("curl" "curl_global_init" "" HAVE_CURL)  #xxx Need 'curl/curl.h'?
-
-#xxxTODO: Test this
 find_package(Boost)
+
+set(THREADS_PREFER_PTHREAD_FLAG TRUE)
+find_package(Threads)
+
+find_package(CURL)
+#xxx No longer needed?
+#check_library_exists("curl" "curl_global_init" "" 	HAVE_CURL)  #xxx Need 'curl/curl.h'?
+
+#find_package(ZLIB::ZLIB)
 
 # Visual Studio 2013 has nullptr but not constexpr.  Need to check for
 # both in here to make sure we have full C++11 support... otherwise,
---
+++ modules/drivers/zlib/coda-oss_minizip.cmake
@@ -0,0 +1,38 @@
+# Some of our code depends on code from a 'minizip' project that is distributed
+#   with zlib.  However, we don't want the whole project; just a couple of 
+#   files, bundled into a library.
+set("OUTPUT_NAME"		"minizip")
+set("TARGET_NAME"		"${CMAKE_PROJECT_NAME}_${OUTPUT_NAME}")
+set("MAINTAINER"		"jmrandol@users-xourceforge.net")
+set("TARGET_VERSION"	"")
+set("MODULE_DEPS"		"zlib")
+
+# The 3P's own build process makes an executable, which we can't re-use.
+# Set up a small custom project to build a library containing the subset 
+# of this 3P's files that we want to reuse.
+
+#xxx Find the zlib dir directly?
+set("source_dir" "${coda-oss_zlib_SOURCE_DIR}/contrib/minizip")
+set("include_dir" "${source_dir}")
+
+set("source_filenames" "ioapi.c" "zip.c")
+set("header_filenames" "ioapi.h" "zip.h")
+
+#xxx With CMake 3.12, we should be able to use the following instead:
+#list(TRANSFORM ${source_filenames} PREPEND ${source_dir})
+string(REGEX REPLACE "([^;]+)" "${source_dir}/\\1" source_fullpaths "${source_filenames}")
+string(REGEX REPLACE "([^;]+)" "${include_dir}/\\1" header_fullpaths "${header_filenames}")
+
+add_library("${TARGET_NAME}" ${source_fullpaths})
+target_sources("${TARGET_NAME}" PUBLIC "${header_fullpaths}")
+set_target_properties("${TARGET_NAME}" PROPERTIES OUTPUT_NAME "${OUTPUT_NAME}")
+target_link_libraries("${TARGET_NAME}" "${MODULE_DEPS}")
+target_include_directories("${TARGET_NAME}" PUBLIC "${include_dir}")
+
+install(TARGETS "${TARGET_NAME}"
+	LIBRARY DESTINATION ${CODA_STD_PROJECT_LIB_DIR}
+	ARCHIVE DESTINATION ${CODA_STD_PROJECT_LIB_DIR}
+	RUNTIME DESTINATION ${CODA_STD_PROJECT_BIN_DIR}
+)
+
+install(FILES ${header_fullpaths} DESTINATION ${CODA_STD_PROJECT_INCLUDE_DIR})
