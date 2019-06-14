7c9b4d89792f89ed1156afa89d982a29b968e2ac
JonathanMeans <jonathan.means@mdaus.com>
2019-06-14 09:40:35 -0400

    Fix module name again

--- modules/python/io/unittests/test_string_conversions.py
+++ modules/python/io/unittests/test_string_conversions.py
@@ -2,12 +2,12 @@
 
 """
  * =========================================================================
- * This file is part of io-c++
+ * This file is part of io-python
  * =========================================================================
  *
  * (C) Copyright 2019, MDA Information Systems LLC
  *
- * io-c++ is free software; you can redistribute it and/or modify
+ * io-python is free software; you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
  * the Free Software Foundation; either version 3 of the License, or
  * (at your option) any later version.
