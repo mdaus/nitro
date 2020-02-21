918b3f5468fcb5d53390abe382f388846c8b78a7
christianwhite1193 <christian.white@radiantsolutions.com>
2020-02-21 09:30:17 -0500

    polygon --> types

--- modules/c++/types/source/RangeList.cpp
+++ modules/c++/types/source/RangeList.cpp
@@ -4,7 +4,7 @@
  *
  * (C) Copyright 2004 - 2020, Radiant Geospatial Solutions
  *
- * polygon-c++ is free software; you can redistribute it and/or modify
+ * types-c++ is free software; you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
  * the Free Software Foundation; either version 3 of the License, or
  * (at your option) any later version.
