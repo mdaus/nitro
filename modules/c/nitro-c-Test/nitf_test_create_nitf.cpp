90b3a1bd4946dc31dfa45fa65693dca9c69133ed
Dan Smith <dan.smith@maxar.com>
2020-07-28 10:32:35 -0400

    build & run "test_create_nitif"

--- modules/c/nitro-c-Test/nitf_test_create_nitf.cpp
+++ modules/c/nitro-c-Test/nitf_test_create_nitf.cpp
@@ -2,5 +2,11 @@
 
 #include "nitf_Test.h"
 
-#define TEST_CASE(X) TEST(nitf_test_create_nitf, X)
+#define TEST_NAME nitf_test_create_nitf
+#define TEST_CASE(X) TEST(TEST_NAME, X)
+
+int argc = 1;
+const char* argv[] = { "nitf_test_create_nitf" };
+#define TEST_CASE_ARGS(X) TEST(TEST_NAME, X)
+
 #include "../nitf/unittests/test_create_nitf.c"
