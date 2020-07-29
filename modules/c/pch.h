39e32f2847866b3ce90d435e01b7dc6a45a70090
Dan Smith <dan.smith@maxar.com>
2020-07-29 15:41:09 -0400

    enable pre-compiled headers

---
+++ modules/c/pch.h
@@ -0,0 +1,32 @@
+#pragma once
+
+#pragma warning(disable: 4668) // '...' is not defined as a preprocessor macro, replacing with '...' for '...'
+#pragma warning(disable: 4820) // '...': '...' bytes padding added after data member '...'
+#pragma warning(disable: 4710) // '...': function not inlined
+#pragma warning(disable: 4255) // '...': no function prototype given : converting '...' to '...'
+
+#pragma warning(disable: 5045) // Compiler will insert Spectre mitigation for memory load if / Qspectre switch specified
+
+// TODO: get rid of these someday?
+#pragma warning(disable: 4100) // '...': unreferenced formal parameter
+#pragma warning(disable: 4296) // '...': expression is always false
+#pragma warning(disable: 4267) // '...': conversion from '...' to '...', possible loss of data
+#pragma warning(disable: 4244) // 	'...': conversion from '...' to '...', possible loss of data
+#pragma warning(disable: 4242) // '...': conversion from '...' to '...', possible loss of data
+#pragma warning(disable: 4018) // '...': signed / unsigned mismatch
+#pragma warning(disable: 4389) // '...': signed / unsigned mismatch
+
+#   include <assert.h>
+#   include <stdlib.h>
+#   include <string.h>
+#   include <stdio.h>
+#   include <math.h>
+#   include <stdarg.h>
+#   include <sys/types.h>
+#   include <ctype.h>
+#   include <time.h>
+#include <memory.h>
+
+#pragma warning(disable: 4505) // '...': unreferenced local function has been removed
+
+#define NRT_LIB_VERSION "2.9"
