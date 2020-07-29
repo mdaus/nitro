4e54ec0abe161d936774fd56c7ee7214b63f5190
Dan Smith <dan.smith@maxar.com>
2020-07-29 15:18:34 -0400

    pre-compiled header file for nitro-c++

---
+++ modules/c++/pch.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#include <stdio.h>
+
+#include <string>
+#include <sstream>
+#include <numeric>
+#include <limits>
+#include <memory>
+#include <algorithm>
+
+#include "import/nitf.h"
+
+#include "nitf/Convert.h"
+#include "nitf/Semaphore.h"
+#include <sys/File.h>
+#include <mem/ScopedArray.h>
+#include <mem/SharedPtr.h>
+#include <except/Exception.h>
+
+
