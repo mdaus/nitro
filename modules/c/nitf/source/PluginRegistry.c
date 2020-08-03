61c3984500995d132da29bf043131de07ddb312b
Dan Smith <dan.smith@maxar.com>
2020-08-03 14:01:30 -0400

    get WIN32 right

--- modules/c/nitf/source/PluginRegistry.c
+++ modules/c/nitf/source/PluginRegistry.c
@@ -32,12 +32,11 @@ insertCreator(nitf_DLL* dso,
               const char* suffix,
               nitf_Error* error);
 
-#ifndef WIN32
+static long __PluginRegistryInitLock = 0;
 static nitf_Mutex __PluginRegistryLock = NITF_MUTEX_INIT;
+#if !(defined(WIN32) || defined(_WIN32))
 static const char DIR_DELIMITER = '/';
 #else
-static nitf_Mutex __PluginRegistryLock = NULL;
-static long __PluginRegistryInitLock = 0;
 static const char DIR_DELIMITER = '\\';
 #endif
 /*
@@ -46,7 +45,7 @@ static const char DIR_DELIMITER = '\\';
  *
  */
 
-#ifdef WIN32
+#if defined(WIN32) || defined(_WIN32)
 NITFPRIV(nitf_Mutex*) GET_MUTEX()
 {
     if (__PluginRegistryLock == NULL)
@@ -891,9 +890,6 @@ insertCreator(nitf_DLL* dso,
               const char* suffix,
               nitf_Error* error)
 {
-    /*  We are trying to find tre_main  */
-    NITF_DLL_FUNCTION_PTR dsoMain = NULL;
-
     /*  Get the name of the handler  */
     char name[NITF_MAX_PATH];
 
@@ -916,8 +912,9 @@ insertCreator(nitf_DLL* dso,
     printf("Loading function [%s] in dso at [%p]\n", name, dso);
 #endif
 
+    /*  We are trying to find tre_main  */
     /*  Retrieve the main  */
-    dsoMain = nitf_DLL_retrieve(dso, name, error);
+    NITF_DLL_FUNCTION_PTR dsoMain  = nitf_DLL_retrieve(dso, name, error);
 
     if (!dsoMain)
     {
@@ -932,7 +929,7 @@ insertCreator(nitf_DLL* dso,
     }
 #endif
 
-    return nitf_HashTable_insert(hash, ident, dsoMain, error);
+    return nitf_HashTable_insert(hash, ident, (NITF_DATA*)dsoMain, error);
 }
 
 /*
