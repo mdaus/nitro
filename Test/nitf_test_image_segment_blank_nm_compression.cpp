fbd048966afe24196f19c924c934376ce3970398
Dan Smith <dan.smith@maxar.com>
2020-08-10 15:18:14 -0400

    enable three more C++ unit-tests

--- Test/nitf_test_image_segment_blank_nm_compression.cpp
+++ Test/nitf_test_image_segment_blank_nm_compression.cpp
@@ -3,5 +3,4 @@
 #include "nitf_Test.h"
 
 #define TEST_CASE(X) TEST(test_image_segment_blank_nm_compression, X)
-//#include "nitf/unittests/test_image_segment_blank_nm_compression.cpp" // TODO
-
+#include "nitf/unittests/test_image_segment_blank_nm_compression.cpp"
