646309ae0824da1d27d20517ffc9417ac518f7ca
Dan Smith <Dan.Smith@maxar.com>
2020-08-31 11:51:46 -0400

    new Test_ project -- trying to get GTest to work w/new VS install

---
+++ Test_/pch.h
@@ -0,0 +1,43 @@
+//
+// pch.h
+// Header for standard system include files.
+//
+
+#pragma once
+
+#include <assert.h>
+#include <stdlib.h>
+#include <math.h>
+#include <stdarg.h>
+#include <ctype.h>
+#include <time.h>
+#include <stdint.h>
+
+#include <string>
+#include <ios>
+
+#include <windows.h>
+#undef min
+#undef max
+#  include <direct.h>
+#  include <io.h>
+# include <sys/types.h>
+# include <sys/stat.h>
+
+extern int close(int fd);
+extern int read(int fd, void* buf, unsigned int count);
+extern int write(int fd, const void* buf, unsigned int count);
+extern int _isatty(int fd);
+#include "gtest/gtest.h"
+
+#include <import/nrt.h>
+
+#include "nitf_Test.h"
+#include "Test.h"
+
+#pragma comment(lib, "io-c++")
+#pragma comment(lib, "except-c++")
+#pragma comment(lib, "sys-c++")
+#pragma comment(lib, "str-c++")
+
+#pragma comment(lib, "ws2_32")
