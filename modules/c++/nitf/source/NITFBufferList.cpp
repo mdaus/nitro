a2c5b1b2b85915ed8801316424c53157ae951308
Dan Smith <dan.smith@maxar.com>
2020-09-12 16:08:32 -0400

    Revert "Merge branch 'develop/master' into develop/jdsmith-VS2019"
    This reverts commit 1ebecf391d8f84d8cb6d2ca583df010b367ca440, reversing
    changes made to 3b30a54fb8dd46ad055e3c4a1a9ec3347c63d709.

--- modules/c++/nitf/source/NITFBufferList.cpp
+++ modules/c++/nitf/source/NITFBufferList.cpp
@@ -29,7 +29,7 @@
 namespace nitf
 {
 NITFBuffer::NITFBuffer() :
-    mData(NULL),
+    mData(nullptr),
     mNumBytes(0)
 {
 }
@@ -84,7 +84,7 @@ size_t NITFBufferList::getNumBytesInBlock(
 
 const void* NITFBufferList::getBlock(size_t blockSize,
                                      size_t blockIdx,
-                                     std::vector<sys::byte>& scratch,
+                                     std::vector<std::byte>& scratch,
                                      size_t& numBytes) const
 {
     const size_t startByte = blockIdx * blockSize;
@@ -101,8 +101,8 @@ const void* NITFBufferList::getBlock(size_t blockSize,
             const size_t numBytesLeftInBuffer =
                     buffer.mNumBytes - numBytesToSkip;
 
-            const sys::byte* const startPtr =
-                    static_cast<const sys::byte*>(buffer.mData) +
+            const std::byte* const startPtr =
+                    static_cast<const std::byte*>(buffer.mData) +
                     numBytesToSkip;
             if (numBytesLeftInBuffer >= numBytes)
             {
@@ -144,6 +144,6 @@ const void* NITFBufferList::getBlock(size_t blockSize,
     }
 
     // Should not be possible to get here
-    return NULL;
+    return nullptr;
 }
 }
