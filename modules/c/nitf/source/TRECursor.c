21b0c37f9d3fc1e167fa547381578cae1beef2a5
Dan Smith <dan.smith@maxar.com>
2020-09-14 16:58:44 -0400

    our own strcat_s() routine

--- modules/c/nitf/source/TRECursor.c
+++ modules/c/nitf/source/TRECursor.c
@@ -137,7 +137,7 @@ NITFAPI(nitf_TRECursor) nitf_TRECursor_clone(nitf_TRECursor *tre_cursor,
 
     cursor.prev_ptr = tre_cursor->prev_ptr;
     cursor.desc_ptr = tre_cursor->desc_ptr;
-    strcpy(cursor.tag_str, tre_cursor->tag_str);
+    nrt_strcpy_s(cursor.tag_str, NITF_TRECursor_tag_str_LEN, tre_cursor->tag_str);
     cursor.length = tre_cursor->length;
     return cursor;
 }
@@ -180,7 +180,7 @@ NITFPRIV(nitf_Pair *) nitf_TRECursor_getTREPair(nitf_TRE * tre,
         while ((bracePtr = strchr(bracePtr + 1, '[')) != NULL)
         {
             /* tack on the depth */
-            strcat(tag_str, idx_str[index++]);
+            nrt_strcat_s(tag_str, TAG_BUF_LEN, idx_str[index++]);
         }
     }
     else
@@ -193,7 +193,7 @@ NITFPRIV(nitf_Pair *) nitf_TRECursor_getTREPair(nitf_TRE * tre,
                 ((nitf_TREPrivateData*)tre->priv)->hash, tag_str);
         for (i = 0; i < looping && !pair; ++i)
         {
-            strcat(tag_str, idx_str[i]);
+            nrt_strcat_s(tag_str, TAG_BUF_LEN, idx_str[i]);
             pair = nitf_HashTable_find(
                     ((nitf_TREPrivateData*)tre->priv)->hash, tag_str);
         }
@@ -309,7 +309,7 @@ NITFAPI(int) nitf_TRECursor_iterate(nitf_TRECursor * tre_cursor,
                     {
                         char entry[64];
                         NITF_SNPRINTF(entry, 64, "[%d]", stack[index]);
-                        strcat(tre_cursor->tag_str, entry);
+                        nrt_strcat_s(tre_cursor->tag_str, NITF_TRECursor_tag_str_LEN, entry);
                     }
                 }
 
@@ -540,7 +540,7 @@ NITFPRIV(int) nitf_TRECursor_evalLoops(nitf_TRE* tre,
         {
             assert(strlen(desc_ptr->label) < sizeof(str));
 
-            strcpy(str, desc_ptr->label);
+            nrt_strcpy_s(str, TAG_BUF_LEN, desc_ptr->label);
             op = str;
             while (isspace(*op))
                 op++;
@@ -639,7 +639,7 @@ NITFPRIV(int) nitf_TRECursor_evalIf(nitf_TRE* tre,
     field = (nitf_Field *) pair->data;
     assert(strlen(desc_ptr->label) < sizeof(str));
 
-    strcpy(str, desc_ptr->label);
+    nrt_strcpy_s(str, TAG_BUF_LEN, desc_ptr->label);
     op = str;
 
     while (isspace(*op))
