e2bb1946e12f103a1f6c95ccd4f947d9f5354422
Dan Smith <dan.smith@maxar.com>
2020-09-15 13:06:02 -0400

    restore VS2019 files

--- modules/c/j2k/source/OpenJPEGImpl.c
+++ modules/c/j2k/source/OpenJPEGImpl.c
@@ -20,8 +20,8 @@
  *
  */
 
-#undef J2K_MODULE_EXPORTS
-#include "j2k/j2k_config.h"
+ //#include "j2k/j2k_config.h"
+//#define HAVE_OPENJPEG_H 1
 
 #ifdef _MSC_VER // Visual Studio
 #pragma warning(disable: 4206) //	nonstandard extension used : translation unit is empty
--- modules/c/nrt/include/nrt/Types.h
+++ modules/c/nrt/include/nrt/Types.h
@@ -24,7 +24,8 @@
 #define __NRT_TYPES_H__
 #pragma once
 
-#include "nrt/nrt_config.h"
+//#include "nrt/nrt_config.h"
+#define NRT_LIB_VERSION "2.9"
 
 #   include <assert.h>
 #   include <stdlib.h>
