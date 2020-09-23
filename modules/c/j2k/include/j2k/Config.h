3496d43542fefa0f1bde9b16988a07597c2ad377
Dan Smith <dan.smith@maxar.com>
2020-09-23 17:00:24 -0400

    build j2k routines in VS2019

--- modules/c/j2k/include/j2k/Config.h
+++ modules/c/j2k/include/j2k/Config.h
@@ -1,10 +1,14 @@
 #pragma once
 
+#undef J2K_MODULE_EXPORTS
+
 #if (defined(WIN32) || defined(_WIN32)) && defined(NITRO_PCH)
+
+#define HAVE_OPENJPEG_H 
+#define J2K_MODULE_EXPORTS 
 
 #else
 
-#undef J2K_MODULE_EXPORTS
 #include "j2k/j2k_config.h"
 
 #endif
