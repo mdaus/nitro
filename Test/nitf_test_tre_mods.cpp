f60c96aca5db00c86e8b3720d17734d5011ceacc
Dan Smith <dan.smith@maxar.com>
2020-12-07 18:19:36 -0500

    sys::Filesystem -> std::filesystem

--- Test/nitf_test_tre_mods.cpp
+++ Test/nitf_test_tre_mods.cpp
@@ -7,7 +7,7 @@
 
 #include "nitf_Test.h"
 
-namespace fs = sys::Filesystem;
+namespace fs = std::filesystem;
 
 static bool is_x64_Configuration(const fs::path& path) // "Configuration" is typically "Debug" or "Release"
 {
