9ca83b609693367d6f40f96bc7674b927b2d2119
Dan Smith <dan.smith@maxar.com>
2020-12-07 18:04:29 -0500

    update coda-oss

--- externals/coda-oss/modules/c++/sys/include/sys/Conf.h
+++ externals/coda-oss/modules/c++/sys/include/sys/Conf.h
@@ -358,4 +358,19 @@ namespace sys
 
 }
 
-#endif
+#define CODA_OSS_cplusplus __cplusplus
+// MSVC only sets __cplusplus >199711L with the /Zc:__cplusplus command-line option.
+// https://devblogs.microsoft.com/cppblog/msvc-now-correctly-reports-__cplusplus/
+#if (CODA_OSS_cplusplus <= 199711L)  // We need at least C++11
+    #undef CODA_OSS_cplusplus  // oops...try to fix
+    #if defined(_MSVC_LANG)
+    // https://docs.microsoft.com/en-us/cpp/preprocessor/predefined-macros?view=msvc-160
+    // "Defined as an integer literal that specifies the C++ language standard targeted by the compiler."
+    #define CODA_OSS_cplusplus _MSVC_LANG
+    #else
+    #define CODA_OSS_cplusplus 201103L  // C++11
+    #endif  // _MSVC_LANG
+#endif  // CODA_OSS_cplusplus
+static_assert(CODA_OSS_cplusplus >= 201103L, "Must compile with C++11 or greater.");
+
+#endif // __SYS_CONF_H__
