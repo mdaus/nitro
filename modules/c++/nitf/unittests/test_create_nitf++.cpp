35064693727dd4671682075239c26e4960f0ed08
Dan Smith <dan.smith@maxar.com>
2020-12-07 12:36:22 -0500

    make test_mt_record a unit-test

--- modules/c++/nitf/unittests/test_create_nitf++.cpp
+++ modules/c++/nitf/unittests/test_create_nitf++.cpp
@@ -32,6 +32,8 @@
  */
 
 #include <string>
+#include <sstream>
+#include <thread>
 
 #include <io/FileOutputStream.h>
 #include <import/sys.h>
@@ -1371,9 +1373,86 @@ TEST_CASE(test_create_nitf_test)
     }
 }
 
+
+struct RecordThread final : public sys::Thread
+{
+    RecordThread() {}
+    virtual ~RecordThread() {}
+    //static sys::Mutex m;
+
+    virtual void run()
+    {
+        nitf::Record record(NITF_VER_21);
+        nitf::Writer writer;
+        nitf::FileHeader header = record.getHeader();
+        header.getFileHeader().set("NITF");
+        header.getComplianceLevel().set("09");
+        header.getSystemType().set("BF01");
+        header.getOriginStationID().set("Bckyd");
+        header.getFileTitle().set("FTITLE");
+        header.getClassification().set("U");
+        header.getMessageCopyNum().set("00000");
+        header.getMessageNumCopies().set("00000");
+        header.getEncrypted().set("0");
+        header.getBackgroundColor().setRawData((char*)"000", 3);
+        header.getOriginatorName().set("");
+        header.getOriginatorPhone().set("");
+        const std::string name = "ACFTB";
+        //m.lock();
+        (void) new nitf::TRE(name, name);
+
+        std::string file;
+        {
+            std::stringstream ss;
+            ss << std::this_thread::get_id();
+            file = ss.str() + ".ntf";
+        }
+
+        nitf::IOHandle output(file, NITF_ACCESS_WRITEONLY, NITF_CREATE);
+        writer.prepare(output, record);
+
+        writer.write();
+    }
+
+};
+
+TEST_CASE(test_mt_record)
+{
+    const int NTHR = 2;
+    
+    try
+    {
+        sys::Thread** thrs = new sys::Thread * [NTHR];
+        for (unsigned int i = 0; i < NTHR; ++i)
+        {
+            thrs[i] = new RecordThread();
+            thrs[i]->start();
+        }
+
+        for (unsigned int i = 0; i < NTHR; ++i)
+        {
+
+            thrs[i]->join();
+            delete thrs[i];
+
+        }
+        delete[] thrs;
+    }
+    catch (const except::Exception&)
+    {
+        TEST_ASSERT_TRUE(false);
+    }
+
+    TEST_ASSERT_TRUE(true);
+}
+
+
+
+
 TEST_MAIN(
     (void)argc;
     (void)argv;
     TEST_CHECK(test_create_nitf_with_byte_provider_test);
     TEST_CHECK(test_create_nitf_test);
+    TEST_CHECK(test_mt_record);
 )
