0515e0bcfb83a8cc037ef4cc7198a460ddcf7403
Dan Smith <dan.smith@maxar.com>
2020-12-15 11:06:39 -0500

    trying to turn test_writer_3++ into a unittest

---
+++ modules/c++/nitf/unittests/test_writer_3++.cpp
@@ -0,0 +1,238 @@
+/* =========================================================================
+ * This file is part of NITRO
+ * =========================================================================
+ *
+ * (C) Copyright 2004 - 2014, MDA Information Systems LLC
+ *
+ * NITRO is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this program; if not, If not,
+ * see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#include <vector>
+#include <iostream>
+#include <string>
+
+#include <sys/Filesystem.h>
+
+#include <import/nitf.hpp>
+#include <gsl/gsl.h>
+
+#include "TestCase.h"
+
+namespace fs = std::filesystem;
+
+/*
+ * This test tests the round-trip process of taking an input NITF
+ * file and writing it to a new file. This includes writing the image
+ * segments (headers, extensions, and image data). This is an example
+ * of how users can write the image data to their NITF file
+ */
+
+static std::string argv0;
+static fs::path findInputFile()
+{
+    const fs::path inputFile = fs::path("modules") / "c++" / "nitf" / "unittests" / "sicd_50x50.nitf";
+    if (argv0.empty())
+    {
+        auto root = fs::current_path().parent_path().parent_path();
+        // running in Visual Studio
+        return root / inputFile;
+    }
+
+    std::clog << argv0 << "\n";
+    return ""; // TODO
+}
+
+static nitf::Record doRead(const std::string& inFile);
+
+static std::string makeBandName(const std::string& rootFile, int imageNum, int bandNum)
+{
+    std::string::size_type pos = rootFile.find_last_of("/\\");
+    std::ostringstream os;
+    os << rootFile.substr(pos + 1) << "__" << imageNum << "_band_" << bandNum;
+    std::string newFile = os.str();
+
+    while ((pos = newFile.find(".")) != std::string::npos)
+        newFile.replace(pos, 1, "_");
+    newFile += ".man";
+    return newFile;
+}
+
+static nitf::ImageSource setupBands(int nbands, int imageNum, const std::string& inRootFile)
+{
+    nitf::ImageSource iSource;
+    for (int i = 0; i < nbands; i++)
+    {
+        std::string inFile = makeBandName(inRootFile, imageNum, i);
+        nitf::FileSource fs(inFile, 0, 1, 0);
+        iSource.addBand(fs);
+    }
+    return iSource;
+}
+
+static void doWrite(nitf::Record record, const std::string& inRootFile, const std::string& outFile)
+{
+    nitf::Writer writer;
+    nitf::IOHandle output(outFile, NITF_ACCESS_WRITEONLY, NITF_CREATE);
+    writer.prepare(output, record);
+
+    int numImages = record.getHeader().getNumImages();
+    nitf::ListIterator end = record.getImages().end();
+    nitf::ListIterator iter = record.getImages().begin();
+
+    for (int i = 0; i < numImages && iter != end; ++i, ++iter)
+    {
+        nitf::ImageSegment imseg;
+        imseg = *iter;
+        int nbands = imseg.getSubheader().getNumImageBands();
+        nitf::ImageWriter iWriter = writer.newImageWriter(i);
+        nitf::ImageSource iSource = setupBands(nbands, i, inRootFile);
+        iWriter.attachSource(iSource);
+    }
+    writer.write();
+    output.close();
+}
+
+static std::string testName;
+
+TEST_CASE(test_writer_3)
+{
+    ::testName = testName;
+
+    std::string input_file = findInputFile().string();
+    const std::string output_file = "test_writer_3++.nitf";
+
+    // Check that wew have a valid NITF
+    const auto version = nitf::Reader::getNITFVersion(input_file);
+    TEST_ASSERT(version != NITF_VER_UNKNOWN);
+
+    nitf::Record record = doRead(input_file);
+    doWrite(record, input_file, output_file);
+}
+
+static void manuallyWriteImageBands(nitf::ImageSegment & segment,
+                             const std::string& imageName,
+                             nitf::ImageReader& deserializer,
+                             int imageNumber)
+{
+    int padded;
+
+    nitf::ImageSubheader subheader = segment.getSubheader();
+
+    uint32_t nBits = subheader.getNumBitsPerPixel();
+    uint32_t nBands = subheader.getNumImageBands();
+    uint32_t xBands = subheader.getNumMultispectralImageBands();
+    nBands += xBands;
+
+    uint32_t nRows = subheader.getNumRows();
+    uint32_t nColumns = subheader.getNumCols();
+
+    //one row at a time
+    const auto subWindowSize = gsl::narrow<size_t>(nColumns * NITF_NBPP_TO_BYTES(nBits));
+
+    TEST_ASSERT_EQ(2, nBands);
+    TEST_ASSERT_EQ(0, xBands);
+    TEST_ASSERT_EQ(50, nRows);
+    TEST_ASSERT_EQ(50, nColumns);
+
+    std::cout << "PVTYPE -> " << subheader.getPixelValueType().toString() << std::endl
+        << "NBPP -> " << subheader.getNumBitsPerPixel().toString() << std::endl
+        << "ABPP -> " << subheader.getActualBitsPerPixel().toString() << std::endl
+        << "PJUST -> " << subheader.getPixelJustification().toString() << std::endl
+        << "IMODE -> " << subheader.getImageMode().toString() << std::endl
+        << "NBPR -> " << subheader.getNumBlocksPerRow().toString() << std::endl
+        << "NBPC -> " << subheader.getNumBlocksPerCol().toString() << std::endl
+        << "NPPBH -> " << (int)subheader.getNumPixelsPerHorizBlock() << std::endl
+        << "NPPBV -> " << (int)subheader.getNumPixelsPerVertBlock() << std::endl
+        << "IC -> " << subheader.getImageCompression().toString() << std::endl
+        << "COMRAT -> " << subheader.getCompressionRate().toString() << std::endl;
+
+    std::vector<uint8_t*> buffer(nBands);
+    std::vector<std::unique_ptr<uint8_t[]>> buffer_(nBands);
+    std::vector<uint32_t> bandList(nBands);
+
+    for (uint32_t band = 0; band < nBands; band++)
+        bandList[band] = band;
+
+    nitf::SubWindow subWindow;
+    subWindow.setStartCol(0);
+    subWindow.setNumRows(1);
+    subWindow.setNumCols(nColumns);
+
+    // necessary ?
+    std::unique_ptr<nitf::DownSampler> pixelSkip(new nitf::PixelSkip(1, 1));
+    subWindow.setDownSampler(pixelSkip.get());
+    subWindow.setBandList(bandList.data());
+    subWindow.setNumBands(nBands);
+
+    for (uint32_t i = 0; i < nBands; i++)
+    {
+        buffer_[i].reset(new uint8_t[subWindowSize]);
+        buffer[i] = buffer_[i].get();
+    }
+
+    std::vector<nitf::IOHandle> handles;
+    //make the files
+    for (int i = 0; i < gsl::narrow<int>(nBands); i++)
+    {
+        std::string name = makeBandName(imageName, imageNumber, i);
+        nitf::IOHandle toFile(name, NITF_ACCESS_WRITEONLY, NITF_CREATE);
+        handles.push_back(toFile);
+    }
+
+    //read all row blocks and write to disk
+    for (uint32_t i = 0; i < nRows; ++i)
+    {
+        subWindow.setStartRow(i);
+        deserializer.read(subWindow, buffer.data(), &padded);
+        for (uint32_t j = 0; j < nBands; j++)
+        {
+            handles[j].write(buffer[j], subWindowSize);
+        }
+    }
+
+    //close output handles
+    for (uint32_t i = 0; i < nBands; i++)
+        handles[i].close();
+}
+
+static nitf::Record doRead(const std::string& inFile)
+{
+    nitf::Reader reader;
+    nitf::IOHandle io(inFile);
+    nitf::Record record = reader.read(io);
+
+    /*  Set this to the end, so we'll know when we're done!  */
+    nitf::ListIterator iter = record.getImages().begin();
+
+    const uint32_t num = record.getNumImages();
+    for (int i = 0; i < gsl::narrow<int>(num); i++)
+    {
+        nitf::ImageSegment imageSegment = *iter;
+        iter++;
+        nitf::ImageReader deserializer = reader.newImageReader(i);
+
+        /*  Write the thing out  */
+        manuallyWriteImageBands(imageSegment, inFile, deserializer, i);
+    }
+
+    return record;
+}
+
+TEST_MAIN(
+    (void)argc;
+    argv0 = argv[0];
+    //TEST_CHECK(test_writer_3);
+)
