f8912752a67ed7593744272d4a4ea9f91dd9c302
Dan Smith <dan.smith@maxar.com>
2021-01-04 13:59:21 -0500

    latest from coda-oss/main

--- externals/coda-oss/modules/c++/gsl/include/gsl/gsl.h
+++ externals/coda-oss/modules/c++/gsl/include/gsl/gsl.h
@@ -1,3 +1,28 @@
+/* =========================================================================
+ * This file is part of gsl-c++
+ * =========================================================================
+ *
+ * (C) Copyright 2004 - 2014, MDA Information Systems LLC
+ *
+ * gsl-c++ is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this program; If not,
+ * see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#ifndef CODA_OSS_gsl_h_INCLUDED_
+#define CODA_OSS_gsl_h_INCLUDED_
+
 #pragma once
 
 #include "sys/Conf.h"
@@ -9,7 +34,7 @@
 
 #if !CODA_OSS_use_gsl_
 
-#include "gsl_.h" // our own "fake" GSL
+#include "Gsl_.h" // our own "fake" GSL
 
 #else
 
@@ -23,3 +48,5 @@
 #include "string_span"		// zstring, string_span, zstring_builder...
 
 #endif
+
+#endif  // CODA_OSS_gsl_h_INCLUDED_
