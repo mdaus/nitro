e6448b1dd469f1b647144064829c74536cb3c0b9
J. Daniel Smith <dan.smith@maxar.com>
2021-01-11 18:29:46 -0500

    tweaks so that six builds with nitro updates (#299)
    * std::byte -> nitf::byte until we can use C++17 everywhere
    
    * mem::SharedPtr -> std::shared_ptr is a breaking change

--- modules/c++/nitf/include/nitf/IOStreamWriter.hpp
+++ modules/c++/nitf/include/nitf/IOStreamWriter.hpp
@@ -45,7 +45,7 @@ public:
      *
      *  \param stream The stream to use for writing
      */
-    IOStreamWriter(std::shared_ptr<io::SeekableOutputStream> stream);
+    IOStreamWriter(mem::SharedPtr<io::SeekableOutputStream> stream);
 
 private:
     void readImpl(void* buffer, size_t size);
@@ -64,7 +64,7 @@ private:
 
     void closeImpl();
 
-    std::shared_ptr<io::SeekableOutputStream> mStream;
+    mem::SharedPtr<io::SeekableOutputStream> mStream;
 };
 }
 
