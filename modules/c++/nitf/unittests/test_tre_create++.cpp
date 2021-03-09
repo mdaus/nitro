f682e252fd7f200d374e032b1fcca7dca1fd224d
Dan Smith <dan.smith@maxar.com>
2021-03-09 18:26:15 -0500

    a NULL descriptionName is OK and should not be a NITF_FAILURE

--- modules/c++/nitf/unittests/test_tre_create++.cpp
+++ modules/c++/nitf/unittests/test_tre_create++.cpp
@@ -19,16 +19,19 @@ TEST_CASE(test_tre_clone_329)
     // https://github.com/mdaus/nitro/issues/329
     const std::string rd = "begin1020030004ABCDEFend";
 
-    nitf_Error error;
-    auto rawTre = nitf_TRE_construct("TESTxyz", NITF_TRE_RAW, &error);
-    nitf_TRE_setField(rawTre, "raw_data", const_cast<char*>(rd.c_str()), rd.size(), &error);
+    nitf_TRE* rawTre = nullptr;
+    {
+        nitf_Error error;
+        rawTre = nitf_TRE_construct("TESTxyz", NITF_TRE_RAW, &error);
+        TEST_ASSERT_NOT_EQ(nullptr, rawTre);
+
+        nitf_TRE_setField(rawTre, "raw_data", const_cast<char*>(rd.c_str()), rd.size(), &error);
+        TEST_ASSERT_TRUE(true);
+    }
+
+    const nitf::TRE tre(rawTre);
     TEST_ASSERT_TRUE(true);
-
-    nitf::TRE tre(rawTre);
-    TEST_ASSERT_TRUE(true);
-
-    nitf::TRE cloneTre = tre.clone();  // leads to free(): invalid size(): in libc.so.6 when the program exits; worked prior to 2.10
-
+    const auto cloneTre = tre.clone();  // leads to free(): invalid size(): in libc.so.6 when the program exits; worked prior to 2.10
     TEST_ASSERT_TRUE(true);
 }
 
