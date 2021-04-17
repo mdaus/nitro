4b0062b73474bff299b45a2419b19625ddc35e56
Dan Smith <Dan.Smith@maxar.com>
2021-04-17 15:52:21 -0400

    pragmas only for MSVC

--- modules/c++/nitf/include/nitf/coda-oss.hpp
+++ modules/c++/nitf/include/nitf/coda-oss.hpp
@@ -22,8 +22,8 @@
 
 #pragma once
 
+#if _MSC_VER
 #pragma warning(push)
-
 #pragma warning(disable: 26447) // The function is declared '...' but calls function '..' which may throw exceptions (f.6).
 #pragma warning(disable: 26455) // Default constructor may not throw. Declare it '...' (f.6).
 #pragma warning(disable: 26440) // Function '...' can be declared '...' (f.6).
@@ -31,12 +31,14 @@
 #pragma warning(disable: 26433) // Function '...' should be marked with '...' (c.128).
 #pragma warning(disable: 26481) // Don't use pointer arithmetic. Use span instead (bounds.1).
 #pragma warning(disable: 26487) // Don't return a pointer '...' that may be invalid (lifetime.4).
+#endif // _MSC_VER
 
 #include <std/bit> // std::endian
 #include <std/cstddef> // std::byte
 #include <std/filesystem>
 #include <std/span>
 
+#if _MSC_VER
 #pragma warning(disable: 26432) // If you define or delete any default operation in the type '...', define or delete them all(c.21).
 #pragma warning(disable: 26440) // Function '...' can be declared '...' (f.6).
 
@@ -52,6 +54,7 @@
 #pragma warning(disable: 26409) // Avoid calling new and delete explicitly, use std::make_unique<T> instead (r.11).
 #pragma warning(disable: 26456) // Operator '...' hides a non-virtual operator '...' (c.128).
 #pragma warning(disable: 26429) // Symbol '...' is never tested for nullness, it can be marked as not_null (f.23).
+#endif // _MSC_VER
 
 //#define CODA_OSS_Throwable_isa_std_exception 1 // except::Throwable derives from std::exception
 #include <sys/Conf.h>
@@ -59,12 +62,15 @@
 
 #include <import/str.h>
 #include <import/sys.h>
+#if _MSC_VER
 #pragma warning(push)
 #pragma warning(disable: 5039) //	'...': pointer or reference to potentially throwing function passed to 'extern "C"' function under - EHc.Undefined behavior may occur if this function throws an exception.
+#endif // _MSC_VER
 #include <import/mt.h>
+#if _MSC_VER
 #pragma warning(pop)
+#endif // _MSC_VER
 #include <import/io.h>
-#include <import/logging.h>
 
 #include <import/mt.h>
 #include <import/sys.h>
@@ -74,4 +80,6 @@
 #include <import/mem.h>
 #include <gsl/gsl.h>
 
+#if _MSC_VER
 #pragma warning(pop)
+#endif // _MSC_VER
