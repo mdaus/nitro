2380778dd764db716c935f10060af86ae20d103d
Dan Smith <dan.smith@maxar.com>
2021-08-27 09:14:38 -0400

    Squashed commit of the following:
    commit 904b7f79520b8cfd86e07731d9e2bcd5f5245cfe
    Author: Dan Smith <dan.smith@maxar.com>
    Date:   Fri Aug 27 09:13:52 2021 -0400
    
        overloads are preferred over specializations
    
    commit f1d24a71b9e4e4aa1f4fc85591189359578e8f4f
    Author: Dan Smith <dan.smith@maxar.com>
    Date:   Fri Aug 27 09:04:25 2021 -0400
    
        getFieldValue()
    
    commit b7a543fd10d1544bc50b7c18824f439027a6a527
    Author: Dan Smith <dan.smith@maxar.com>
    Date:   Fri Aug 27 08:50:15 2021 -0400
    
        setField() -> setFieldValue()
    
    commit d712bcdca66f306f76a2e4feab5e88844c94aff1
    Author: Dan Smith <dan.smith@maxar.com>
    Date:   Thu Aug 26 14:03:25 2021 -0400
    
        don't call truncate() for binary field
    
    commit b8ba47b2e625e3076d6d1c326047b70747ea46e6
    Author: Dan Smith <dan.smith@maxar.com>
    Date:   Thu Aug 26 13:47:49 2021 -0400
    
        fixing TRE::setField() for "const char*"
    
    commit a6a18845c8c0d534965fede4e4612156f8769958
    Author: Dan Smith <dan.smith@maxar.com>
    Date:   Thu Aug 26 12:18:53 2021 -0400
    
        fix broken TRE::setField()
    
    commit 219d581039f0cdd6566a20df519c1212c3a5edfe
    Author: Dan Smith <dan.smith@maxar.com>
    Date:   Thu Aug 26 10:28:22 2021 -0400
    
        update ENGRDA to use ENGDATA[0]
    
    commit 6c96c2572de776570c4dccdb407b4d3d413d0c30
    Author: Dan Smith <dan.smith@maxar.com>
    Date:   Thu Aug 26 08:23:55 2021 -0400
    
        fix errors building with older compilers
    
    commit 2ae24213831e15da330588b27874c5796326b16e
    Author: Dan Smith <dan.smith@maxar.com>
    Date:   Wed Aug 25 17:53:38 2021 -0400
    
        unit-test for https://github.com/mdaus/nitro/discussions/394
    
    commit 62f05fc0f947f2c4886d0a51ad675d9e45cf571c
    Author: Dan Smith <dan.smith@maxar.com>
    Date:   Wed Aug 25 15:20:54 2021 -0400
    
        testing the ENGRDA TRE

--- modules/c++/nitf/include/nitf/TRE.hpp
+++ modules/c++/nitf/include/nitf/TRE.hpp
@@ -169,7 +169,8 @@ struct NITRO_NITFCPP_API TREFieldIterator : public nitf::Object<nitf_TREEnumerat
  */
 DECLARE_CLASS(TRE)
 {
-    void setField(const nitf_Field&, const std::string & key, const std::string & data, bool forceUpdate);
+    nitf_Field& nitf_TRE_getField(const std::string&) const;
+    void setFieldValue(const nitf_Field&, const std::string & tag, const std::string & data, bool forceUpdate);
 
 public:
     typedef nitf::TREFieldIterator Iterator;
@@ -200,7 +201,11 @@ public:
 
     // for unit-tests
     static nitf_TRE* create(const std::string & tag, const std::string & id, nitf_Error& error) noexcept;
-    static bool setField(nitf_TRE * tre, const std::string & tag, const std::string& data, nitf_Error& error) noexcept;
+    static bool setFieldValue(nitf_TRE*, const std::string & tag, const std::string& data, nitf_Error&) noexcept;
+    static bool setField(nitf_TRE* tre, const std::string & tag, const std::string & data, nitf_Error & error) noexcept
+    {
+        return setFieldValue(tre, tag, data, error);
+    }
 
     //! Clone
     nitf::TRE clone() const;
@@ -271,30 +276,57 @@ public:
      *                    of the TRE fields. See `updateFields()`
      */
 
-    void setField(const std::string& key, const std::string& value, bool forceUpdate = false);
-    void setField(const std::string & key, const char* value, bool forceUpdate = false);
-    void setField(const std::string& key, const void* data, size_t dataLength, bool forceUpdate = false);
+    // TRE fields use some of the "field" infrastructure, but have their own API.
+
+    void setFieldValue(const std::string& tag, const std::string& value, bool forceUpdate);
+    void setFieldValue(const std::string & tag, const char* value, bool forceUpdate);
+    void setFieldValue(const std::string& tag, const void* data, size_t dataLength, bool forceUpdate);
 
     // This is wrong when T is "const char*" and the field is NITF_BINARY; sizeof(T) won't make sense.
+    // That's why there is a "const char*" overload above.
     template <typename T>
-    void setField(const std::string& key, T value, bool forceUpdate = false)
+    void setFieldValue(const std::string& tag, const T& value, bool forceUpdate)
     {
-        const nitf_Field* const field = nitf_TRE_getField(getNative(), key.c_str());
-        if (!field)
+        const auto& field = nitf_TRE_getField(tag);
+        if (field.type == NITF_BINARY)
         {
-            std::ostringstream msg;
-            msg << key << " is not a recognized field for this TRE";
-            throw except::Exception(Ctxt(msg.str()));
-        }
-        if (field->type == NITF_BINARY)
-        {
-            setField(key, &value, sizeof(value), forceUpdate);
+            setFieldValue(tag, &value, sizeof(value), forceUpdate);
         }
         else
         {
-            setField(*field, key, str::toString(value), forceUpdate);
+            setFieldValue(field, tag, str::toString(value), forceUpdate);
         }
     }
+
+    template <typename T>
+    void setField(const std::string& tag, const T& value, bool forceUpdate = false)
+    {
+        setFieldValue(tag, value, forceUpdate);
+    }
+    void setField(const std::string& tag, const void* data, size_t dataLength, bool forceUpdate = false)
+    {
+        setFieldValue(tag, data, dataLength, forceUpdate);
+    }
+
+    template<typename T>
+    const T& getFieldValue(const std::string& tag, T& value) const
+    {
+        value = static_cast<T>(getField(tag));
+        return value;
+    }
+    const std::string& getFieldValue(const std::string& tag, std::string& value, bool trim = false) const
+    {
+        value = getField(tag).toString(trim);
+        return value;
+    }
+    template<typename T>
+    const T getFieldValue(const std::string& tag) const
+    {
+        T retval;
+        getFieldValue(tag, retval);
+        return retval;
+    }
+
 
     /*!
      *  Does the field exist?
