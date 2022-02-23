466534fd855dab4accd33979bc2c7bac9719946c
Dan Smith <dan.smith@maxar.com>
2022-02-23 17:07:44 -0500

    begin work in C++ wrappers for 'j2k' C API.

---
+++ Test++/nitf_test_j2k_loading++.cpp
@@ -0,0 +1,10 @@
+#include "pch.h"
+
+#include <windows.h>
+#include <combaseapi.h>
+#undef interface
+
+#include "nitf_Test.h"
+
+#define TEST_CASE(X) TEST(test_j2k_loading__, X)
+#include "nitf/unittests/test_j2k_loading++.cpp"
