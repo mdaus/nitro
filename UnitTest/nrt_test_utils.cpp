d4a82b169725e3c17d61993e209866e398dcd2d0
J. Daniel Smith <dan@jdanielsmith.me>
2022-03-11 12:02:30 -0500

    us VS "unit tests" as they don't need extra stuff

---
+++ UnitTest/nrt_test_utils.cpp
@@ -0,0 +1,12 @@
+#include "pch.h"
+
+#include "nrt_Test.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+TEST_CLASS(nrt_test_utils) { public:
+		
+#define TEST_CASE(X) TEST_METHOD(X)
+#include "nrt/unittests/test_utils.c"
+
+};
