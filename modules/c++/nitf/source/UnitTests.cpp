1122e5051fb12118ddb79e3d7eda54de868d85ce
J. Daniel Smith <dan@jdanielsmith.me>
2022-03-16 18:08:59 -0400

    reduce unused code

--- modules/c++/nitf/source/UnitTests.cpp
+++ modules/c++/nitf/source/UnitTests.cpp
@@ -53,30 +53,6 @@ static std::string PlatformToolset()
 #endif
 }
 
-static bool is_x64_Configuration(const fs::path& path) // "Configuration" is typically "Debug" or "Release"
-{
-	static const std::string build_configuration = Configuration();
-	const auto Configuration = path.filename();
-	const auto path_parent_path = path.parent_path();
-	const auto x64 = path_parent_path.filename();
-	return (Configuration == build_configuration) && (x64 == Platform());
-}
-
-static bool is_install_unittests(const fs::path& path)
-{
-	const auto unittests = path.filename();
-	const auto path_parent_path = path.parent_path();
-	const auto install = path_parent_path.filename();
-	return (unittests == "unittests") && (install == "install");
-}
-static bool is_install_tests(const fs::path& path)
-{
-	const auto tests = path.filename();
-	const auto path_parent_path = path.parent_path();
-	const auto install = path_parent_path.filename();
-	return (tests == "tests") && (install == "install");
-}
-
 static fs::path make_waf_install(const fs::path& p)
 {
 	// just "install" on Linux; install-Debug-x64.v142 on Windows
@@ -120,16 +96,11 @@ static fs::path buildDir(const fs::path& path)
 
 	const auto exec = fs::absolute(fs::path(os.getCurrentExecutable()));
 	const auto argv0 = exec.filename();
-	if ((argv0 == "Test++.exe") || (argv0 == "testhost.exe"))
+
+	if (argv0 == "testhost.exe")
 	{
-		// Running GTest unit-tests in Visual Studio on Windows
-		if (is_x64_Configuration(cwd))
-		{
-			//const auto root = cwd.parent_path().parent_path();
-			//const auto install = "install-" + Configuration() + "-" + Platform() + "." + PlatformToolset();
-			//return root / install / path;
-			return cwd / path;
-		}
+		// Running in Visual Studio on Windows
+		return cwd / path;
 	}
 
 	auto extension = argv0.extension().string();
@@ -137,13 +108,6 @@ static fs::path buildDir(const fs::path& path)
 	if (extension == ".EXE")
 	{
 		// stand-alone executable on Windows (ends in .EXE)
-		const auto parent_path = exec.parent_path();
-		if (is_x64_Configuration(parent_path))
-		{
-			const auto parent_path_ = parent_path.parent_path().parent_path();
-			return parent_path_ / "dev" / "tests" / "images";
-		}
-
 		const auto root = findRoot(exec);
 		std::string relative_exec = exec.string();
 		str::replaceAll(relative_exec, root.string(), "");
@@ -158,30 +122,6 @@ static fs::path buildDir(const fs::path& path)
 		}
 		
 		return install / path;
-	}
-
-	// stand-alone unit-test on Linux
-	const auto exec_dir = exec.parent_path();
-	if (is_install_unittests(exec_dir))
-	{
-		const auto install = exec_dir.parent_path();
-		return install / "unittests" / "data";
-	}
-	if (is_install_tests(exec_dir))
-	{
-		const auto install = exec_dir.parent_path();
-		return install / "unittests" / "data";
-	}
-
-	if (argv0 == "unittests")
-	{
-		// stand-alone unittest executable on Linux
-		const auto bin = exec.parent_path();
-		if (bin.filename() == "bin")
-		{
-			const auto unittests = bin.parent_path();
-			return unittests / "unittests" / "data";
-		}
 	}
 
 	//fprintf(stderr, "cwd = %s\n", cwd.c_str());
