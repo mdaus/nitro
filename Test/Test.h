e9b611e239ab16064612cefaff12cf9677abf42b
J. Daniel Smith <dan@jdanielsmith.me>
2022-04-01 22:38:16 -0400

    build cleanly in VS2022

--- Test/Test.h
+++ Test/Test.h
@@ -14,13 +14,13 @@ inline std::wstring ToString(const uint16_t& q)
 }
 }}}
 
-#define TEST_ASSERT(X) Assert::IsTrue(X)
+#define TEST_ASSERT(X) Assert::IsTrue((X) ? true : false)
 
 template<typename T, typename U>
 inline void test_assert_eq_(T&& t, U&& u)
 {
-	Assert::AreEqual(t, u);
-	Assert::AreEqual(u, t);
+	Microsoft::VisualStudio::CppUnitTestFramework::Assert::AreEqual(t, u);
+	Microsoft::VisualStudio::CppUnitTestFramework::Assert::AreEqual(u, t);
 }
 #define TEST_ASSERT_EQ(X1, X2) test_assert_eq_(X1, X2);
 #define TEST_ASSERT_EQ_INT(X1, X2) TEST_ASSERT_EQ(X2, X1)
@@ -35,18 +35,18 @@ inline void test_assert_eq_(T&& t, U&& u)
 template<typename T, typename U>
 inline void test_assert_not_eq_(T&& t, U&& u)
 {
-	Assert::AreNotEqual(t, u);
-	Assert::AreNotEqual(u, t);
+	Microsoft::VisualStudio::CppUnitTestFramework::Assert::AreNotEqual(t, u);
+	Microsoft::VisualStudio::CppUnitTestFramework::Assert::AreNotEqual(u, t);
 }
 #define TEST_ASSERT_NOT_EQ(X1, X2) test_assert_not_eq_(X1, X2);
 template<typename T, typename U>
 inline void test_assert_greater_(T&& t, U&& u)
 {
-	Assert::IsTrue(t > u);
+	Microsoft::VisualStudio::CppUnitTestFramework::Assert::IsTrue(t > u);
 }
 #define TEST_ASSERT_GREATER(X1, X2) test_assert_greater_(X1, X2)
 
-#define TEST_ASSERT_ALMOST_EQ_EPS(X1, X2, EPS) { Assert:AreEqual(X1, X2, EPS); Assert:AreEqual(X2, X1, EPS); }
+#define TEST_ASSERT_ALMOST_EQ_EPS(X1, X2, EPS) { Microsoft::VisualStudio::CppUnitTestFramework::Assert:AreEqual(X1, X2, EPS); Microsoft::VisualStudio::CppUnitTestFramework::Assert:AreEqual(X2, X1, EPS); }
 #define TEST_ASSERT_ALMOST_EQ(X1, X2) TEST_ASSERT_ALMOST_EQ_EPS(X1, X2, 0.0001)
 
 #define TEST_ASSERT_EQ_MSG(msg, X1, X2) Logger::WriteMessage(msg.c_str()); TEST_ASSERT_EQ(X1, X2)
