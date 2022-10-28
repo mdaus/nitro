47c951bcc64031440fe4cba9f949098906fd1f3c
Dan Smith <dan.smith@maxar.com>
2022-10-28 09:27:38 -0400

    #pragma warning is non-standard

--- externals/coda-oss/modules/c++/avx/include/avx/extractf.h
+++ externals/coda-oss/modules/c++/avx/include/avx/extractf.h
@@ -26,7 +26,7 @@
 
 // Not supported on GCC < 7.1; see https://gcc.gnu.org/bugzilla/show_bug.cgi?id=80322 (also 80323-80325)
 #if ((__GNUC__) && (GCC_VERSION < 70100))
-#pragma warning Disabling CODA AVX m256 support due to gcc compiler bug.
+//#pragma warning Disabling CODA AVX m256 support due to gcc compiler bug.
 #else
 
 #include <config/compiler_extensions.h>
