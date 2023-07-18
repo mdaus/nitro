60b3c0f0daca81fe13d4c9273da38744c9a3f0f1
Dan Smith <dan.smith@maxar.com>
2023-07-18 09:35:21 -0400

    Fixed bug in Error::to_string(); using the entire buffer rather than to '\0'

--- externals/coda-oss/modules/c++/except/include/except/Throwable.h
+++ externals/coda-oss/modules/c++/except/include/except/Throwable.h
@@ -166,8 +166,8 @@ public:
         if (t.getSize() > 0)
             s << ": " << t;
 
-        std::string retval(s.str().c_str()); // truncate to first '\0'
-        return retval;
+        return s.str();
+        //return s.str().c_str(); // truncate to first '\0'
     }
 
     const std::vector<std::string>& getBacktrace() const noexcept
