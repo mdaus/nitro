b97c4a8678884465055e2b79c00eefbb5ca60724
J. Daniel Smith <dan.smith@maxar.com>
2023-11-15 10:03:44 -0500

    disable pre-loaded TREs (#597)
    * merge develop/preload-TREs
    
    * fix build errors
    
    * #including some TREs
    
    * "export" TRE routines as "static"
    
    * struct for pre-loaded TREs rather than "void*"
    
    * start working on resolving preloaded TREs
    
    * retrievePreloadedTREHandler() to match nitf_PluginRegistry_retrieveTREHandler()
    
    * preload a TRE
    
    * preload several TREs
    
    * fix duplicate defs
    
    * fix compiler warning
    
    * use macros to remove multiple-defined symbols
    
    * keep a few TREs as plugins for testing
    
    * preloaded TRE stuff needs to be in PluginHandler so that nitf_PluginRegistry_retrieveTREHandler() "just works"
    
    * remove another multiple-defined symbols
    
    * assume plug-in that fails is "preloaded"
    
    * remove VCXPROJ for TREs that are preloaded
    
    * don't preload ANY TREs to preserve existing behavior
    
    * be sure all nrt_DLL fields are initialized
    
    * revert whitespace changes
    
    * added a preloaded TEST_PRELOADED_DES TRE
    
    * the TRE id could have underscores in it, so search from the end.
    
    * preload TREs, but disable them
    
    * allow preloaded TREs to be enabled/disabled
    
    * allow all TREs to be enabled (or disabled)
    
    * don't need to call setNitfPluginPath() as much with preloaded TREs
    
    * always build TEST_DES as a DLL
    
    * strongly-typed TEST_DES TRE

--- UnitTest/UnitTest.cpp
+++ UnitTest/UnitTest.cpp
@@ -1,11 +1,14 @@
 #include "pch.h"
 #include "CppUnitTest.h"
 
+#include <nitf/PluginRegistry.h>
 #include <nitf/UnitTests.hpp>
 
 // https://learn.microsoft.com/en-us/visualstudio/test/microsoft-visualstudio-testtools-cppunittestframework-api-reference?view=vs-2022
 TEST_MODULE_INITIALIZE(methodName)
 {
+    nitf_PluginRegistry_PreloadedTREHandlersEnable(NRT_TRUE);
+
     // module initialization code
-    nitf::Test::setNitfPluginPath();
+    nitf::Test::j2kSetNitfPluginPath();
 }
