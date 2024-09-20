96e610567932bf9260540f46ae9d85c99132f690
abeaucha <adam.beauchamp@radiantsolutions.com>
2024-09-20 11:07:36 -0400

    Squashed 'externals/coda-oss/' changes from c92a55d7c..2352e4859
    2352e4859 target macros better
    ce98bcd1a fix C4127: Conditional Expression is Constant
    bed75961d disable AVX512 for MSVC < 1920
    2ebe52f68 Release 2024-03-18 (#771)
    463cb15ae Make it easier to turn off SIMD support (#770)
    a6f8cc400 determine UTF-8 at compile-time (#768)
    586249d49 use lookup tables; remove "strict" processing (#767)
    3add73906 Merge branch 'main' of github.com:mdaus/coda-oss
    3ea6e1958 remove sys::Ximd (#766)
    bfdc4b1ea std::execution::par is slow with GCC ...
    d723792a2 Build cleanly with cmake "release" (#765)
    625b2860f settings unused of we have std::transform(par
    82d54124a add back transform_async() as Transform_par() (#764)
    63659eede provide a simple implementation of std::experimental::simd (#763)
    59a47c70e update to latest versions of various actions (#762)
    f98c13c38 add missing #include
    b173830ca fix C++17 overload resolution
    ed283d676 Only need to actually create/destroy Xerces once (#761)
    8087b1e11 utility routines to convert to/from collections of paths
    a869c8a8e use a lookup table for str::upper() and str::lower() (#760)
    00e343177 Merge branch 'main' of https://github.com/mdaus/coda-oss
    27085f06b DefaultErrorHandler is a base class
    98ec79835 favor speed over size
    28926b673 provide our own (simple) mdspan implementation (#759)
    6bb722454 make SpanRC API match std::mdspan (#758)
    df4e0fc1f changes to build SIX DLLs (#757)
    faf5d0521 allow CMake to switch between building SHARED (DLLs) or STATIC libraires
    d15173f38 export symbols used by NITRO (#756)
    6ed3c048b use Debug DLL library in Debug
    5eae4c1f6 turn of /Wall for MSVC (#755)
    47d4b6b73 return type needs to be explicit (not `auto`) for subsequent specialization
    aea8e7b1e reduce use of str::toString() (#754)
    8062154a4 HighFive 2,8,0 (#753)
    92f8b88c7 std::ostringstream overloads
    8cb27de27 reduce use of .c_str() and .str() (#752)
    3ae8f6afb Merge pull request #751 from mdaus/bugfix/hdf5-missing-files-main
    3ec510861 Add missing files
    1693eac7e Update build_unittest.yml
    03f1a3bdf release 2023-10-23 (#747)
    4dd7b2fbd Merge branch 'main' of github.com:mdaus/coda-oss
    9428f12a0 add extensions for .log and known Windows/Linux binaries (#746)
    c3fd20a88 added some OLD Visual Studio extensions
    d00db384a everything is text except for known binaries
    c3813bb29 eol=lf is the default
    4a2f472c7 add extensions for .log and known Windows/Linux binaries (#746)
    18f2c051f more FmtX() -> str::Format() changes (#745)
    959532681 reduce use of FmtX macro (#743)
    fffac7fc4 Fix memory leaks in "cli" (#741)
    95ff879ba make it easier to turn on AVX2/AVX512F (#740)
    a950c848b enable AVX2 and AVX512F CMake builds (#739)
    369737085 use std::ssize() to reduce casts (#738)
    8ae7dabac std::size() and std::ssize() (#737)
    8db480be5 enable ASAN for GitHub builds (#736)
    
    git-subtree-dir: externals/coda-oss
    git-subtree-split: 2352e485929c513fc5f90f44f8fae945aec37b3e

--- modules/c++/sys/source/OSWin32.cpp
+++ modules/c++/sys/source/OSWin32.cpp
@@ -59,7 +59,7 @@ std::string sys::OSWin32::getPlatformName() const
         platform = "Unknown Windows OS";
     }
     auto retval = platform + ": ";
-    retval += FmtX("%d.%d [build: %d], ", info.dwMajorVersion, info.dwMinorVersion, info.dwBuildNumber);
+    retval += str::Format("%d.%d [build: %d], ", info.dwMajorVersion, info.dwMinorVersion, info.dwBuildNumber);
     retval += info.szCSDVersion;
     return retval;
 }
@@ -112,7 +112,7 @@ void sys::OSWin32::removeFile(const std::string& pathname) const
         oss << "Failure removing file [" <<  pathname <<
             "] with error [" << err.toString() << "]";
 
-        throw except::Exception(Ctxt(oss.str()));
+        throw except::Exception(Ctxt(oss));
     }
 }
 
@@ -125,7 +125,7 @@ void sys::OSWin32::removeDirectory(const std::string& pathname) const
         oss << "Failure removing directory [" <<  pathname <<
             "] with error [" << err.toString() << "]";
 
-        throw except::Exception(Ctxt(oss.str()));
+        throw except::Exception(Ctxt(oss));
     }
 }
 
@@ -357,14 +357,19 @@ void sys::OSWin32::getAvailableCPUs(std::vector<int>& /*physicalCPUs*/,
 sys::SIMDInstructionSet sys::OSWin32::getSIMDInstructionSet() const
 {
     // https://learn.microsoft.com/en-us/windows/win32/api/processthreadsapi/nf-processthreadsapi-isprocessorfeaturepresent
+    // (these require Windows SDK (19041) or later)
+#ifdef PF_AVX512F_INSTRUCTIONS_AVAILABLE
     if (IsProcessorFeaturePresent(PF_AVX512F_INSTRUCTIONS_AVAILABLE))
     {
         return SIMDInstructionSet::AVX512F;
     }
+#endif
+#ifdef PF_AVX2_INSTRUCTIONS_AVAILABLE
     if (IsProcessorFeaturePresent(PF_AVX2_INSTRUCTIONS_AVAILABLE))
     {
         return SIMDInstructionSet::AVX2;
     }
+#endif
     if (IsProcessorFeaturePresent(PF_XMMI64_INSTRUCTIONS_AVAILABLE))
     {
         return SIMDInstructionSet::SSE2;
@@ -414,7 +419,7 @@ void sys::OSWin32::removeSymlink(const std::string& symlinkPathname) const
         oss << "Failure removing symlink [" <<  symlinkPathname <<
             "] with error [" << err.toString() << "]";
 
-        throw except::Exception(Ctxt(oss.str()));
+        throw except::Exception(Ctxt(oss));
     }
 }
 
