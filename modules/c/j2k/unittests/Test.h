eb45c8b3889470ad49b53ec92e7e249e7b16977c
Adam Beauchamp <adam.beauchamp@radiantsolutions.com>
2024-11-18 11:36:23 -0500

    clang-format

--- modules/c/j2k/unittests/Test.h
+++ modules/c/j2k/unittests/Test.h
@@ -25,62 +25,154 @@
 
 #ifdef __cplusplus
 
-#include <string>
-
 #include <import/sys.h>
 
-#define CHECK(X) X(std::string(#X)); std::cout << #X << ": PASSED" << std::endl
-#define TEST_ASSERT(X) if (!(X)) { die_printf("%s (%s,%s,%d): FAILED: Value should not be NULL\n", testName.c_str(), __FILE__, __FUNC__, __LINE__) }
-#define TEST_ASSERT_NULL(X) if ((X) != NULL) { die_printf("%s (%s,%s,%d): FAILED: Value should be NULL\n", testName.c_str(), __FILE__, __FUNC__, __LINE__) }
-#define TEST_ASSERT_EQ(X1, X2) if ((X1) != (X2)) { die_printf("%s (%s,%s,%d): FAILED: Recv'd %s, Expected %s\n", testName.c_str(), __FILE__, __FUNC__, __LINE__, std::to_string(X1).c_str(), std::to_string(X2).c_str()) }
-#define TEST_ASSERT_ALMOST_EQ(X1, X2) if (fabs((X1) - (X2)) > std::numeric_limits<float>::epsilon()) { die_printf("%s (%s,%s,%d): FAILED: Recv'd %s, Expected %s\n", testName.c_str(), __FILE__, __FUNC__, __LINE__, std::to_string(X1).c_str(), std::to_string(X2).c_str()) }
+#include <string>
+
+#define CHECK(X)        \
+    X(std::string(#X)); \
+    std::cout << #X << ": PASSED" << std::endl
+#define TEST_ASSERT(X)                                                  \
+    if (!(X))                                                           \
+    {                                                                   \
+        die_printf("%s (%s,%s,%d): FAILED: Value should not be NULL\n", \
+                   testName.c_str(),                                    \
+                   __FILE__,                                            \
+                   __FUNC__,                                            \
+                   __LINE__)                                            \
+    }
+#define TEST_ASSERT_NULL(X)                                         \
+    if ((X) != NULL)                                                \
+    {                                                               \
+        die_printf("%s (%s,%s,%d): FAILED: Value should be NULL\n", \
+                   testName.c_str(),                                \
+                   __FILE__,                                        \
+                   __FUNC__,                                        \
+                   __LINE__)                                        \
+    }
+#define TEST_ASSERT_EQ(X1, X2)                                        \
+    if ((X1) != (X2))                                                 \
+    {                                                                 \
+        die_printf("%s (%s,%s,%d): FAILED: Recv'd %s, Expected %s\n", \
+                   testName.c_str(),                                  \
+                   __FILE__,                                          \
+                   __FUNC__,                                          \
+                   __LINE__,                                          \
+                   std::to_string(X1).c_str(),                        \
+                   std::to_string(X2).c_str())                        \
+    }
+#define TEST_ASSERT_ALMOST_EQ(X1, X2)                                 \
+    if (fabs((X1) - (X2)) > std::numeric_limits<float>::epsilon())    \
+    {                                                                 \
+        die_printf("%s (%s,%s,%d): FAILED: Recv'd %s, Expected %s\n", \
+                   testName.c_str(),                                  \
+                   __FILE__,                                          \
+                   __FUNC__,                                          \
+                   __LINE__,                                          \
+                   std::to_string(X1).c_str(),                        \
+                   std::to_string(X2).c_str())                        \
+    }
 #define TEST_CASE(X) void X(std::string testName)
 
 #else
 
-#   include <stdlib.h>
-#   include <string.h>
-#   include <stdio.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 /*  Negotiate the 'context'  */
 #define TEST_FILE __FILE__
 #define TEST_LINE __LINE__
 #if defined(__GNUC__)
-#    define TEST_FUNC __PRETTY_FUNCTION__
+#define TEST_FUNC __PRETTY_FUNCTION__
 #elif __STDC_VERSION__ < 199901
-#    define TEST_FUNC "unknown function"
-#else                           /* Should be c99 */
-#    define TEST_FUNC __func__
+#define TEST_FUNC "unknown function"
+#else /* Should be c99 */
+#define TEST_FUNC __func__
 #endif
 
-#define CHECK(X) X(#X); fprintf(stderr, "%s : PASSED\n", #X);
-#define CHECK_ARGS(X) X(#X,argc,argv); fprintf(stderr, "%s : PASSED\n", #X);
-#define TEST_ASSERT(X) if (!(X)) { \
-    fprintf(stderr, "%s (%s,%s,%d) : FAILED: Value should not be NULL\n", testName, TEST_FILE, TEST_FUNC, TEST_LINE); \
-    exit(EXIT_FAILURE); \
-}
-#define TEST_ASSERT_NULL(X) if ((X) != NULL) { \
-    fprintf(stderr, "%s (%s,%s,%d) : FAILED: Value should be NULL\n", testName, TEST_FILE, TEST_FUNC, TEST_LINE); \
-    exit(EXIT_FAILURE); \
-}
-#define TEST_ASSERT_EQ_STR(X1, X2) if (strcmp((X1), (X2)) != 0) { \
-    fprintf(stderr, "%s (%s,%s,%d) : FAILED: Recv'd %s, Expected %s\n", testName, TEST_FILE, TEST_FUNC, TEST_LINE, X1, X2); \
-    exit(EXIT_FAILURE); \
-}
-#define TEST_ASSERT_EQ_INT(X1, X2) if ((X1) != (X2)) { \
-    fprintf(stderr, "%s (%s,%s,%d) : FAILED: Recv'd %d, Expected %d\n", testName, TEST_FILE, TEST_FUNC, TEST_LINE, (int)X1, (int)X2); \
-    exit(EXIT_FAILURE); \
-}
+#define CHECK(X) \
+    X(#X);       \
+    fprintf(stderr, "%s : PASSED\n", #X);
+#define CHECK_ARGS(X)  \
+    X(#X, argc, argv); \
+    fprintf(stderr, "%s : PASSED\n", #X);
+#define TEST_ASSERT(X)                                                \
+    if (!(X))                                                         \
+    {                                                                 \
+        fprintf(stderr,                                               \
+                "%s (%s,%s,%d) : FAILED: Value should not be NULL\n", \
+                testName,                                             \
+                TEST_FILE,                                            \
+                TEST_FUNC,                                            \
+                TEST_LINE);                                           \
+        exit(EXIT_FAILURE);                                           \
+    }
+#define TEST_ASSERT_NULL(X)                                       \
+    if ((X) != NULL)                                              \
+    {                                                             \
+        fprintf(stderr,                                           \
+                "%s (%s,%s,%d) : FAILED: Value should be NULL\n", \
+                testName,                                         \
+                TEST_FILE,                                        \
+                TEST_FUNC,                                        \
+                TEST_LINE);                                       \
+        exit(EXIT_FAILURE);                                       \
+    }
+#define TEST_ASSERT_EQ_STR(X1, X2)                                  \
+    if (strcmp((X1), (X2)) != 0)                                    \
+    {                                                               \
+        fprintf(stderr,                                             \
+                "%s (%s,%s,%d) : FAILED: Recv'd %s, Expected %s\n", \
+                testName,                                           \
+                TEST_FILE,                                          \
+                TEST_FUNC,                                          \
+                TEST_LINE,                                          \
+                X1,                                                 \
+                X2);                                                \
+        exit(EXIT_FAILURE);                                         \
+    }
+#define TEST_ASSERT_EQ_INT(X1, X2)                                  \
+    if ((X1) != (X2))                                               \
+    {                                                               \
+        fprintf(stderr,                                             \
+                "%s (%s,%s,%d) : FAILED: Recv'd %d, Expected %d\n", \
+                testName,                                           \
+                TEST_FILE,                                          \
+                TEST_FUNC,                                          \
+                TEST_LINE,                                          \
+                (int)X1,                                            \
+                (int)X2);                                           \
+        exit(EXIT_FAILURE);                                         \
+    }
 /* TODO use epsilon for comparing floating points */
-#define TEST_ASSERT_EQ_FLOAT(X1, X2) if (fabs((X1) - (X2)) > .0000001f) { \
-    fprintf(stderr, "%s (%s,%s,%d) : FAILED: Recv'd %f, Expected %f\n", testName, TEST_FILE, TEST_FUNC, TEST_LINE, X1, X2); \
-    exit(EXIT_FAILURE); \
-}
+#define TEST_ASSERT_EQ_FLOAT(X1, X2)                                \
+    if (fabs((X1) - (X2)) > .0000001f)                              \
+    {                                                               \
+        fprintf(stderr,                                             \
+                "%s (%s,%s,%d) : FAILED: Recv'd %f, Expected %f\n", \
+                testName,                                           \
+                TEST_FILE,                                          \
+                TEST_FUNC,                                          \
+                TEST_LINE,                                          \
+                X1,                                                 \
+                X2);                                                \
+        exit(EXIT_FAILURE);                                         \
+    }
 
 #define TEST_CASE(X) void X(const char* testName)
-#define TEST_CASE_ARGS(X) void X(const char* testName, int argc, char **argv)
+#define TEST_CASE_ARGS(X) void X(const char* testName, int argc, char** argv)
 
-#define TEST_MAIN(X) int main(int argc, char** argv) { {(void)argc; (void)argv;} X; return 0; }
+#define TEST_MAIN(X)                \
+    int main(int argc, char** argv) \
+    {                               \
+        {                           \
+            (void)argc;             \
+            (void)argv;             \
+        }                           \
+        X;                          \
+        return 0;                   \
+    }
 
 #endif
 
